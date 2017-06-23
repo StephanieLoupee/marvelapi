aec5e3be177d210317dcce6b660ee274199cfc94
Stephanie <stephanie.loupee@libertymutual.com>
2017-06-22 20:27:21 -0400

    added comments and fixed search button name

--- assets/javascript/character.js
+++ assets/javascript/character.js
@@ -1,22 +1,32 @@
 $(function() {
 
-    //console.log()
+
+    //This sets up most of the variables needed to string together the api call
+    //searchtext has to be added after the button-click in order to
+    //capture the input from the front end.
 
     let url = "http://gateway.marvel.com:80/v1/public/characters";
     let apikey = "533c42e7f6805eb58bbc046dfb776ae4";
     let hash = "7eaf3f94d5fbbd6ebed1081c97bc1a5c";
     let ts = "1";
     let limit = 100;
-    let submitButton = $("#submit")
+    let searchButton = $("#search")
 
-
-    submitButton.click (function() {
+    //On click of the search button the search text is captured and rolled up
+    //with the other variables to create the queryParm (used in the call to the api)
+    searchButton.click (function() {
       let charSearch = $("#searchtext").val();
       let queryParm = url + "?nameStartsWith=" + charSearch + "&limit=" + limit + "&apikey=" + apikey + "&hash=" + hash + "&ts=" + ts;
 
+    //this is the call to the api which returns data.  Sets 'response' to
+    //get to the results file so don't have to go through data.data all the time
       $.get(queryParm, function( data ) {
         let response = data.data.results;
         //console.log(response);
+
+    //still within the button-click function, this puts together the path and extension
+    //to provide the full address to the thumbnail image, then adds the image to the table.
+    //Adding the height keeps the size of the table manageable, and consistent
         $.each( response, function( index, char ) {
             let picture = "<img src='" + char.thumbnail.path + "." + char.thumbnail.extension + "' height='90'/>"
 
@@ -29,51 +39,3 @@ $(function() {
       })
     })
 })
-
-
-    //   if (guessText.val() == answer) {
-    //     console.log("the answer was right")
-    //     let newScore = parseInt(score.html()) + parseInt(value.html());
-    //     score.html(newScore);
-    //
-    //   } else {
-    //     console.log("the answer is not right. Guess was " + guessText.val() + " ; answer was " + answer);
-    //   }
-    //
-    // function getCharacters() {
-    //   $.get( "http://gateway.marvel.com:80/v1/public/characters", function( data ) {
-    //       //console.log(data);
-    //       //console.log(data[0].question);
-    //       //console.log(data[0].category.title);
-    //       //console.log(data[0].value);
-    //       console.log(data[0].answer);
-    //       category.html(data[0].category.title);
-    //       value.html(data[0].value);
-    //       question.html(data[0].question);
-    //       answer = data[0].answer;
-    //       //guessText.html("My Answer is: ");
-    //       //setText("My Answer is: ");
-    //
-    //       name="apikey" value="533c42e7f6805eb58bbc046dfb776ae4" />
-    //       <input type="hidden" name="hash" value="7eaf3f94d5fbbd6ebed1081c97bc1a5c" />
-    //       <input type="hidden" name="ts" value="1" />
-    //   })
-    // }
-    //
-    //     poseQuestion();
-    //
-    //     answerButton.click (function() {
-    //       let submittedAnswer = guessText.val();
-    //       //console.log("guessText = " + guessText.val());
-    //       if (guessText.val() == answer) {
-    //         console.log("the answer was right")
-    //         let newScore = parseInt(score.html()) + parseInt(value.html());
-    //         score.html(newScore);
-    //
-    //       } else {
-    //         console.log("the answer is not right. Guess was " + guessText.val() + " ; answer was " + answer);
-    //       }
-    //
-    //       poseQuestion();
-    //
-    //
